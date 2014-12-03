8133b2d28201db2909fdb15b9dd533d2f49c8659
Rachel <rmoshier@gmail.com>
2014-12-02 16:53:20 -0800

    still failing test

--- spec/controllers/post_spec.rb
+++ spec/controllers/post_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 require 'spec_helper'
 
-describe ".index" do
-  it "must have a body" do
-    expect(Post.new(body: nil)).to_not be_valid
+  describe ".validates" do
+    it "must have a body" do
+      expect(Post.new(content: nil)).to_not be_valid
+    end
   end
-end
