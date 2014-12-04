191e02059077a5a5bebaf27744ebd7733e845cdf
Lily Pace <lilyfpace@gmail.com>
2014-12-04 15:43:03 -0800

    stuff

--- db/schema.rb
+++ db/schema.rb
@@ -44,4 +44,11 @@ ActiveRecord::Schema.define(version: 20141204193937) do
     t.string   "uid"
   end
 
+  create_table "userstools", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "tool_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
 end
