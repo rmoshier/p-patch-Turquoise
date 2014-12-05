3ee501d4a89303d0c68a56844f0f937f52261402
Rachel <rmoshier@gmail.com>
2014-12-05 11:37:20 -0800

    cleaning up

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,23 +5,21 @@ class User < ActiveRecord::Base
   validates :email, :name, :password, :uid, presence: true
 
     def self.create_from_omniauth(auth_hash)
-        # Create a user
-        User.create(
-          uid:        auth_hash["uid"].to_s,
-          photo_url: auth_hash["info"]["image"],
-          name:   auth_hash["info"]["name"],
-          email: "rachel@email.com",
-          password: "password!"
-        )
+      User.create(
+        uid:        auth_hash["uid"].to_s,
+        photo_url: auth_hash["info"]["image"],
+        name:   auth_hash["info"]["name"],
+        email: "rachel@email.com",
+        password: "password!"
+      )
     end
 
-      def self.find_or_create_from_omniauth(auth_hash)
-        # Find or create a user
-        if user = User.find_by(uid: auth_hash["uid"].to_s)
-            return user
-        else
-            User.create_from_omniauth(auth_hash)
-        end
+    def self.find_or_create_from_omniauth(auth_hash)
+      if user = User.find_by(uid: auth_hash["uid"].to_s)
+          return user
+      else
+          User.create_from_omniauth(auth_hash)
       end
+    end
 
 end
