004842c5e4cb22b0c93c995afbe290f26015f260
Rachel <rmoshier@gmail.com>
2014-12-05 10:17:04 -0800

    clean up sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,8 @@ class SessionsController < ApplicationController
 
   def create
     auth_hash = request.env['omniauth.auth']
-    # raise
-    # create user - but i have this in user model. call it here
     session[:user_id] = User.find_or_create_from_omniauth(auth_hash).uid
     redirect_to root_path
-    # current_user
-
   end
 
 end
