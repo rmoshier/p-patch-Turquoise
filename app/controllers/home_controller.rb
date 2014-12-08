469c21afcc1ebe13424b083143652720cd4bcceb
Rachel <rmoshier@gmail.com>
2014-12-08 13:18:57 -0800

    weather and picture

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,7 +10,6 @@ class HomeController < ApplicationController
   def weather
     @current = Weather.new("seattle").current_weather
     @forecast = Weather.new("seattle").forecast
-    # raise params.inspect
   end
 
 end
--- app/helpers/temperature_helper.rb
+++ app/helpers/temperature_helper.rb
@@ -1,5 +1,5 @@
 module TemperatureHelper
   def convert(k)
-    ((9/5)*(k-273))+32
+    (((9/5)*(k-273))+32).to_i
   end
 end
