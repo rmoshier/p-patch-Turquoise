3197681d79dc86d967861d694851d3d72150fbad
Lily Pace <lilyfpace@gmail.com>
2014-12-09 14:38:13 -0800

    dsdfd

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,7 +9,7 @@ class HomeController < ApplicationController
     # raise params.inspect
     @tools = Tool.all
     @user = User.find_by(uid: session[:user_id])
-    @posts = Post.all
+    @posts = Post.order(created_at: :desc)
   end
 
   def weather
