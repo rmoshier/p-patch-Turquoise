4e6c9e3315de19cddc93e6a7998c1c2fe58d585c
Rachel <rmoshier@gmail.com>
2014-12-10 15:38:27 -0800

    action mailer working

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -1,5 +1,11 @@
 class PostMailer < ActionMailer::Base
-  default from: "from@example.com"
+  # default from: "from@example.com"
 
-  
+  # make a method that sends a new mail to someone who signs up
+  def email_name
+    mail :subject => "Mandrill rides the Rails!",
+    :to      => "rmoshier@gmail.com",
+    :from    => "you@yourdomain.com"
+  end
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,17 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      :address   => "smtp.mandrillapp.com",
+      :port      => 25, # ports 587 and 2525 are also supported with STARTTLS
+      :enable_starttls_auto => true, # detects and uses STARTTLS
+      :user_name => ENV["MANDRILL_USERNAME"],
+      :password  => ENV["MANDRILL_PASSWORD"], # SMTP password is any valid API key
+      :authentication => 'login', # Mandrill supports 'plain' or 'login'
+      :domain => 'yourdomain.com', # your domain to identify your server when connecting
+      }
 end
